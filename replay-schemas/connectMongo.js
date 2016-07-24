e0aadfb397f57fcbc60e9150d2098bc89884b4b0
Aviad Morshet <aviad10m@gmail.com>
2016-07-24 16:19:23 +0300

    Handled disconnections with Mongo, made auto-reconnecting exp.backoff mechanism
    Upgraded version

--- replay-schemas/connectMongo.js
+++ replay-schemas/connectMongo.js
@@ -3,33 +3,77 @@ var mongoose = require('mongoose'),
 
 mongoose.Promise = Promise;
 
-module.exports = function(host, port, database) {
+var keepAliveInSeconds = 60 * 60 * 24 * 30; // 30 days
+// initialize options
+var _options = {
+	server: {
+		socketOptions: {
+			keepAlive: keepAliveInSeconds
+		},
+		auto_reconnect: true
+	},
+	replset: {
+		socketOptions: {
+			keepAlive: keepAliveInSeconds
+		}
+	}
+};
+
+// used to count connection re-try seconds
+var _connectRetryInSeconds = 0;
+
+module.exports = function(_host, _port, _database) {
 	return new Promise(function(resolve, reject) {
-		var host = host || 'localhost';
-		var port = port || 27017;
-		var database = database || 'replay_dev';
-
-		console.log('Conencting to mongo...', ' Database: ', database, '. URI: ', host + ':' + port + '.');
-
-		var keepAliveInSeconds = 60 * 60 * 24 * 30; // 30 days
-		// initialize options
-		var options = {
-			server: {
-				socketOptions: {
-					keepAlive: keepAliveInSeconds
-				}
-			},
-			replset: {
-				socketOptions: {
-					keepAlive: keepAliveInSeconds
-				}
-			}
-		};
-
+		var host = _host || 'localhost';
+		var port = _port || 27017;
+		var database = _database || 'replay_dev';
 		var uri = 'mongodb://' + host + ':' + port + '/' + database;
+
+		setConnectionListeners(uri, database, host, port);
 		// connect to mongo
-		mongoose.connect(uri, options);
-		console.log('Connected to mongo.');
+		mongoose.connect(uri, _options);
 		resolve();
 	});
+};
+
+function setConnectionListeners(uri, database, host, port) {
+	// set event listeners on connection
+	var connection = mongoose.connection;
+
+	connection.on('connecting', function() {
+		console.log('Connecting to mongo...', 'Database:', database, '. URI:', host + ':' + port + '.');
+	});
+
+	connection.on('error', function(error) {
+		console.error('Error in mongo connection: ' + error);
+		console.log('Disconnecting from mongo...');
+		mongoose.disconnect();
+	});
+	connection.on('connected', function() {
+		console.log('Connected to mongo.');
+	});
+	connection.once('open', function() {
+
+	});
+	connection.on('reconnected', function() {
+		console.log('Mongo has reconnected.');
+	});
+	connection.on('disconnected', function() {
+		console.log('Disconnected from mongo.');
+
+		// retry to connect with exponential backoff in order to off-load mongo
+		exponentialBackoff(function() {
+			mongoose.connect(uri, _options);
+		});
+	});
+}
+
+// perform exponential backoff up to 32 seconds (including)
+function exponentialBackoff(func) {
+	_connectRetryInSeconds = Math.pow(2, _connectRetryInSeconds);
+	// do not allow more than half a second of waiting
+	if (_connectRetryInSeconds > 32) {
+		_connectRetryInSeconds = 1;
+	}
+	setTimeout(func, _connectRetryInSeconds * 1000);
 }
