407e1cd2b17d469b48f434de7357fd4b843f96a5
Aviad Morshet <aviad10m@gmail.com>
2016-07-27 17:02:37 +0300

    Added method to create empty entry id in kaltura-service for testing purposes

--- replay-kaltura-service/index.js
+++ replay-kaltura-service/index.js
@@ -1,119 +1,122 @@
-var kaltura = require('./kaltura'),
-    Promise = require('bluebird');
+var Promise = require('bluebird');
+var kaltura = require('./kaltura');
 
 var KalturaClient;
 
+var kalturaResultCallback = function(resolve, reject) {
+	return function(result) {
+		console.log('Kaltura Callback result:', result);
+		if (result.code) {
+			reject(new Error(result.message));
+		} else {
+			resolve(result);
+		}
+	};
+};
+
 module.exports = {
 
-    initialize: initialize,
+	initialize: initialize,
 
-    getVideo: function(id) {
-        return new Promise(function(resolve, reject) {
-            KalturaClient.media.get(function(result) {
-                if (result.code) {
-                    console.log(result);
-                    reject(new Error(result.message));
-                } else {
-                    resolve(result);
-                }
-            }, id);
-        });
-    },
+	getVideo: function(id) {
+		return new Promise(function(resolve, reject) {
+			KalturaClient.media.get(function(result) {
+				if (result.code) {
+					console.log(result);
+					reject(new Error(result.message));
+				} else {
+					resolve(result);
+				}
+			}, id);
+		});
+	},
 
-    addVideo: function(path) {
-        return new Promise(function(resolve, reject) {
-            // add media entry and then add the actual video content to media entry
-            addMediaEntry()
-                .then(function(mediaEntry) {
-                    return addContentToMedia(mediaEntry.id, path);
-                })
-                .catch(function(err) {
-                    if (err) {
-                        reject(err);
-                    }
-                });
-        });
-    }
-}
+	addVideo: function(path) {
+		return new Promise(function(resolve, reject) {
+			// add media entry and then add the actual video content to media entry
+			addMediaEntry()
+				.then(function(mediaEntry) {
+					return addContentToMedia(mediaEntry.id, path);
+				})
+				.catch(function(err) {
+					if (err) {
+						reject(err);
+					}
+				});
+		});
+	},
+
+	generateMediaEntry: function() {
+		return addMediaEntry();
+	}
+};
 
 function addContentToMedia(entryId, path) {
-    var kalturaResource = new kaltura.vo.KalturaServerFileResource();    
-    kalturaResource.localFilePath = path;
+	var kalturaResource = new kaltura.vo.KalturaServerFileResource();
+	kalturaResource.localFilePath = path;
 
-    return new Promise(function(resolve, reject) {
-        KalturaClient.media.addContent(kalturaResultCallback(resolve, reject), entryId, kalturaResource);
-    });
+	return new Promise(function(resolve, reject) {
+		KalturaClient.media.addContent(kalturaResultCallback(resolve, reject), entryId, kalturaResource);
+	});
 }
 
 function addMediaEntry() {
+	var kalturaMediaEntry = new kaltura.vo.KalturaMediaEntry();
+	kalturaMediaEntry.mediaType = kaltura.kc.enums.KalturaMediaType.VIDEO;
 
-    var kalturaMediaEntry = new kaltura.vo.KalturaMediaEntry();
-    kalturaMediaEntry.mediaType = kaltura.kc.enums.KalturaMediaType.VIDEO;
-
-    return new Promise(function(resolve, reject) {
-        KalturaClient.media.add(kalturaResultCallback(resolve, reject), kalturaMediaEntry);
-    });
+	return new Promise(function(resolve, reject) {
+		KalturaClient.media.add(kalturaResultCallback(resolve, reject), kalturaMediaEntry);
+	});
 }
 
 function initialize() {
+	return new Promise(function(resolve, reject) {
+		var partnerId = process.env.KALTURA_PARTNER_ID,
+			secret = process.env.KALTURA_ADMIN_SECRET,
+			url = process.env.KALTURA_URL;
 
-    return new Promise(function(resolve, reject){
-        var partner_id = process.env.KALTURA_PARTNER_ID,
-            secret = process.env.KALTURA_ADMIN_SECRET,
-            url = process.env.KALTURA_URL;
+		if (!partnerId || !secret || !url) {
+			return reject(new Error('Some parameters are missing.'));
+		}
 
-        if(!partner_id || !secret || !url)
-            throw 'Some parameters are missing.';
+		// store client in global variable for later use
+		KalturaClient = createKalturaClient(partnerId, url);
 
-        // store client in global variable for later use
-        KalturaClient = createKalturaClient(partner_id, url);
+		// create session and store in global variable
+		createSession(partnerId, secret, function(result) {
+			// if code is set there was an error
+			if (result.code) {
+				return reject(new Error(result));
+			}
 
-        // create session and store in global variable
-        createSession(partner_id, secret, function(result) {
-
-            // if code is set there was an error
-            if (result.code) return console.log(result);
-
-            console.log('Got a kaltura session: ', result);
-            var kalturaSession = result;
-            KalturaClient.setSessionId(kalturaSession);
-            resolve();
-        });
-    });
+			console.log('Got a kaltura session: ', result);
+			var kalturaSession = result;
+			KalturaClient.setSessionId(kalturaSession);
+			resolve();
+		});
+	});
 }
 
 // create an admin session with kaltura
 // session is returned to callback, which expects 1 result argument
-function createSession(partner_id, secret, callback) {
-    // admin session
-    var type = kaltura.kc.enums.KalturaSessionType.ADMIN;
-    // 1 year session
-    var expiry = 31622400;
-    // ignored when admin
-    var privileges = null;
-    var ks = KalturaClient.session.start(callback, secret, '', type, partner_id, expiry, privileges);
+function createSession(partnerId, secret, callback) {
+	// admin session
+	var type = kaltura.kc.enums.KalturaSessionType.ADMIN;
+	// 1 year session
+	var expiry = 31622400;
+	// ignored when admin
+	var privileges = null;
+	KalturaClient.session.start(callback, secret, '', type, partnerId, expiry, privileges);
 }
 
-function createKalturaClient(partner_id, url) {
-    // init kaltura configuration
-    var kaltura_conf = new kaltura.kc.KalturaConfiguration(partner_id);
+function createKalturaClient(partnerId, url) {
+	// init kaltura configuration
+	var kalturaConfig = new kaltura.kc.KalturaConfiguration(partnerId);
 
-    kaltura_conf.serviceUrl = url;
+	kalturaConfig.serviceUrl = url;
 
-    // init kaltura client
-    var client = new kaltura.kc.KalturaClient(kaltura_conf);
+	// init kaltura client
+	var client = new kaltura.kc.KalturaClient(kalturaConfig);
 
-    return client;
-}
-
-var kalturaResultCallback = function(resolve, reject) {
-    return function(result){
-        console.trace("Recieved a result: ", result);
-        if (result.code) {
-            reject(new Error(result.message));
-        }
-        else{
-            resolve(result);
-        }
-    };
+	return client;
 }
