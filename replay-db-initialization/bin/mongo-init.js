1b370f6cbde8a7071267e02d07666b520e6e876a
motyros <motyros@gmail.com>
2016-08-04 12:26:05 +0300

    add 'replay-db-initialization' for database initialization scripts (close linnovate/replay-infra#5)

---
+++ replay-db-initialization/bin/mongo-init.js
@@ -0,0 +1,54 @@
+#! /usr/bin/env node
+
+var path = require('path');
+
+var Promise = require('bluebird'),
+	connectMongo = require('replay-schemas/connectMongo');
+
+const MONGO_HOST = process.env.MONGO_HOST,
+	MONGO_PORT = process.env.MONGO_PORT,
+	MONGO_DATABASE = process.env.MONGO_DATABASE,
+	REPLAY_SCHEMA = process.env.REPLAY_SCHEMA,
+	DATA_FILE = process.env.DATA_FILE;
+
+checkInput()
+	.then(connectMongo(MONGO_HOST, MONGO_PORT, MONGO_DATABASE))
+	.then(mongoInit)
+	.then(function() {
+		console.log('Mongo initialization completed successfully.');
+		process.exit(0);
+	})
+	.catch(function(err) {
+		console.log('ERROR: ' + err);
+		process.exit(1);
+	});
+
+function checkInput() {
+	console.log('Mongo host: ', MONGO_HOST);
+	console.log('Mongo port: ', MONGO_PORT);
+	console.log('Mongo database: ', MONGO_DATABASE);
+	console.log('Replay schema: ', REPLAY_SCHEMA);
+	console.log('Data file: ', DATA_FILE);
+
+	if (!MONGO_HOST || !MONGO_PORT || !MONGO_DATABASE || !REPLAY_SCHEMA || !DATA_FILE) {
+		// throw new Error('Mongo-init - ERROR! Bad conection params provided!');
+		return Promise.reject('Bad conection params provided!');
+	}
+	return Promise.resolve();
+}
+
+function mongoInit() {
+	var SchemaObj = require('replay-schemas/' + REPLAY_SCHEMA);
+
+	var dataFilePath = path.join('../data-files', DATA_FILE + '.json');
+	var data = require(dataFilePath);
+
+	return SchemaObj.insertMany(data)
+		.then(function() {
+			console.log('Insertion completed successfully.');
+			return Promise.resolve();
+		})
+		.catch(function(err) {
+			return Promise.reject('Insertion failure! \n' + err);
+		});
+}
