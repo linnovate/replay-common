157378f5cbc7330694c983646c1044fadc4bdc27
oldwisenir <nirklinger471@gmail.com>
2016-10-25 13:48:20 +0300

    adding sourceID and tags fields. removing condition generation method

--- replay-schemas/VideoCompartment.js
+++ replay-schemas/VideoCompartment.js
@@ -1,7 +1,4 @@
 require('mongoose-geojson-schema');
-var xml2js = require('xml2js');
-var Promise = require('bluebird');
-var http = require('http');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -10,6 +7,10 @@ var VideoCompartmentSchema = new Schema({
 	videoId: {
 		type: Schema.Types.ObjectId,
 		ref: 'Video'
+	},
+	sourceId: {
+		type: String,
+		required: true
 	},
 	classificationId: {
 		type: Schema.Types.ObjectId,
@@ -33,6 +34,10 @@ var VideoCompartmentSchema = new Schema({
 		type: Number,
 		required: true
 	},
+	tags: [{
+		type: Schema.Types.ObjectId,
+		ref: 'Tag'
+	}],
 	destination: {
 		type: String,
 		required: true
@@ -44,33 +49,8 @@ var VideoCompartmentSchema = new Schema({
 var VideoCompartment = mongoose.model('VideoCompartment', VideoCompartmentSchema);
 
 module.exports = {
-	VideoCompartment: VideoCompartment,
-	generateCompartmentCondition: function(userCode) {
-		return getUserPermissions(userCode)
-			.then(parseXml)
-			.then(buildQuery);
-	}
+	VideoCompartment: VideoCompartment
 };
-
-function buildQuery(permissions) {
-	return new Promise(function(resolve, reject) {
-		try {
-			var query = { $or: [] };
-			for (i = 0; i < permissions.length; i++) {
-				query.$or.push({
-					destination: permissions[i].id[0]
-				});
-				if (i === permissions.length - 1) {
-					console.log('query: ' + JSON.stringify(query));
-					resolve(query);
-					break;
-				}
-			}
-		} catch (err) {
-			reject(err);
-		}
-	});
-}
 
 function validateGreaterThanStartTime(obj) {
 	if (obj.startTime <= obj.endTime) {
@@ -78,53 +58,4 @@ function validateGreaterThanStartTime(obj) {
 	}
 
 	return true;
-}
-
-function getUserPermissions(userCode) {
-	var PromiseRequest = Promise.method(function(options) {
-		return new Promise(function(resolve, reject) {
-			var request = http.request(options, function(response) {
-				// Bundle the result
-				response.setEncoding('utf8');
-				var responseString = '';
-
-				response.on('data', function(data) {
-					responseString += data;
-				});
-
-				response.on('end', function() {
-					resolve(responseString);
-				});
-			});
-
-			// Handle errors
-			request.on('error', function(error) {
-				console.log('Problem with request:', error.message);
-				reject(error);
-			});
-
-			request.end();
-		});
-	});
-
-	return PromiseRequest({
-		host: process.env.COMPARTMENT_HOST || 'localhost', //
-		port: process.env.COMPARTMENT_PORT || 1337, //
-		path: '/compartment/getCompartment',
-		method: 'GET'
-	});
-}
-
-function parseXml(data) {
-	var parser = new xml2js.Parser();
-	return new Promise(function(resolve, reject) {
-		parser.parseString(data, function(err, result) {
-			if (result === undefined || err) {
-				reject(err);
-			} else {
-				var permissions = result.permissions.allow[0].userPermission;
-				resolve(permissions);
-			}
-		});
-	});
 }
